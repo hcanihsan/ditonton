34ee69ac222a34becc5bd7fb850c78c5c40078e0
hcanihsan <budiihsan19@gmail.com>
2021-11-11 22:33:31 +0700

    some changes on json_reader.dart

--- tv_series/test/json_reader.dart
+++ tv_series/test/json_reader.dart
@@ -5,5 +5,5 @@ String readJson(String name) {
   if (dir.endsWith('/test')) {
     dir = dir.replaceAll('/test', '');
   }
-  return File('$dir/test/$name').readAsStringSync();
+  return File('$dir/tv_series/test/$name').readAsStringSync();
 }
